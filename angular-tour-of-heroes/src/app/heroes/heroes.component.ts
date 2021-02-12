183a4aa5596664c222f87a015cb2913147769e1d
Éric Pinto <ericgrandopinto@gmail.com>
2021-02-11 21:45:57 -0300

    added list and details of users

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from './mock-heroes';
 import { Hero } from './Hero';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+
+  selectedHero?:Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero
+  }
+
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
